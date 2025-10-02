c5af313e6035003fc9323329cec89306d021762a
joseph.c <75019048+Joseph-Cha@users.noreply.github.com>
2025-10-02 10:08:18 +0900

    feat: score 클래스를 한  곳으로 정리

--- lib/score.dart
+++ lib/score.dart
@@ -6,3 +6,12 @@ class Score {
   @override
   String toString() => '점수: $score';
 }
+
+class StudentScore extends Score {
+  final String name;
+
+  StudentScore({required this.name, required super.score});
+
+  @override
+  String toString() => '이름: $name, 점수: $score';
+}
